9f6b908574d9372cd57f4dd68f6f6707b75bb8ea
huequica <harumaki1225@gmail.com>
2019-12-02 13:21:39 +0900

    move public function `search_reply`

--- application/functions.rb
+++ application/functions.rb
@@ -18,9 +18,26 @@ class Functions
     @bot_notification_channel = search_channelid(ENV['BOT_NOTIFICATION_CHANNEL'])
   end
 
+  # runnning functions
+  #
+  # @param [data] incomming `Faye::WebSocket::Client#on :message`
+  def search_reply(data)
+    # Replying to `今日も一日`
+    kyomo_ichinichi(data['channel']) if data['text'] == '今日も一日'
+
+    # notify when emojis published
+    if data['type'] == 'emoji_changed'
+      notify_adding_emoji(data['name']) if data['subtype'] == 'add'
+    end
+
+  end
+
   ### sending zoi_get
   #
   # @param [websocket] websocket connection instance.(`Faye::WebSocket::Client`)
+
+  private
+
   def kyomo_ichinichi(channel)
     @websocket_connection.send(
       {
@@ -34,26 +51,14 @@ class Functions
   ### notifying specified channnel at `bot_notification_channel`
   #
   # @param [data] incomming data(`Faye::WebSocket::Client#on :message`)
+
   def notify_adding_emoji(emoji_name)
-    websocket_connection.send(
+    @websocket_connection.send(
       {
         type: 'message',
         text: "New Emojis Published! :#{emoji_name}:",
         channel: @bot_notification_channel
       }.to_json
     )
-  end
-
-  # runnning functions
-  #
-  # @param [data] incomming data(`Faye::WebSocket::Client#on :message`)
-  def search_reply(data)
-    # Replying to `今日も一日`
-    kyomo_ichinichi(data['channel']) if data['text'] == '今日も一日'
-
-    # notify when emojis published
-    if data['type'] == 'emoji_changed'
-      notify_adding_emoji(data['name']) if data['subtype'] == 'add'
-    end
   end
 end
