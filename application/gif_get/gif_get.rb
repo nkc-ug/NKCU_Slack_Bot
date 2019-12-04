b6722d52ebae4906b38e0b55b3e943df4fee7acf
huequica <harumaki1225@gmail.com>
2019-12-04 12:01:56 +0900

    escape empty

--- application/gif_get/gif_get.rb
+++ application/gif_get/gif_get.rb
@@ -4,10 +4,15 @@ def gif_get(search_query)
   require 'http'
 
   giphy_api_key = ENV['GIPHY_API_KEY']
-  response = HTTP.get('https://api.giphy.com/v1/gifs/search',
-                      params: { api_key: giphy_api_key,
-                                q: search_query,
-                                limit: 50 })
-
-  JSON.parse(response)['data'].sample['images']['original']['url']
+  response = JSON.parse(HTTP.get('https://api.giphy.com/v1/gifs/search',
+                                 params: { api_key: giphy_api_key,
+                                           q: search_query,
+                                           limit: 50 }))
+  if response['data'].empty?
+    'SearchCount: 0'
+  else
+    response['data'].sample['images']['original']['url']
+  end
 end
+
+pp gif_get('aiueo700')
