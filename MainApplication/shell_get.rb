5b404feb9970ed1f802b7f8071f9e2a6ef1174e6
huequica <harumaki1225@gmail.com>
2020-04-16 15:40:31 +0900

    inner function

--- MainApplication/shell_get.rb
+++ MainApplication/shell_get.rb
@@ -1,12 +1,16 @@
-#frozen_string_literal:true
+# frozen_string_literal:true
 
 require 'net/http'
 require 'json'
 
-uri_at_shellgei = URI.parse('http://shellgei:4567')
-parameter = { command: 'ojichat' }
-# add Query parameter
-uri_at_shellgei.query = URI.encode_www_form(parameter)
+def shell_get(command)
+  uri_at_shellgei = URI.parse('http://shellgei:4567')
+  parameter = { command: command }
+  # add Query parameter
+  uri_at_shellgei.query = URI.encode_www_form(parameter)
 
-response_object = JSON.parse(Net::HTTP.get(uri_at_shellgei))
-puts response_object['response']
+  response_object = JSON.parse(Net::HTTP.get(uri_at_shellgei))
+
+  # return response key object
+  response_object['response']
+end
