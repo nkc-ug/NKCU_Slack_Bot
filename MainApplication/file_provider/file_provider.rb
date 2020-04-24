4846132465def9e80d702a6420726e2280d24d3d
huequica <harumaki1225@gmail.com>
2020-04-24 16:44:31 +0900

    create file download function

---
+++ MainApplication/file_provider/file_provider.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal:true
+
+require 'open-uri'
+
+# download file to `download_images/` directory
+# @param endpoint <string> image file link
+def download_file(endpoint)
+  # 1. 'https://pbs.twimg.com/media/BspTawrCEAAwQnP.jpg:large' => 'media/BspTawrCEAAwQnP.jpg'
+  # 2. 'media/BspTawrCEAAwQnP.jpg' => 'BspTawrCEAAwQnP.jpg'
+  filename = endpoint[/media\/.*.jpg/][6..]
+
+  URI.open(endpoint) do |binary|
+    File.open("#{__dir__}/../download_images/#{filename}", mode = 'w'){|file|
+      file.write(binary.read)
+    }
+  end
+rescue OpenURI::HTTPError
+  'リソース取得に失敗しました'
+end
