a7d1f11b047645575344fa303603867f3ab459bc
huequica <harumaki1225@gmail.com>
2020-04-24 22:10:45 +0900

    implement file upload function

--- MainApplication/file_provider/file_provider.rb
+++ MainApplication/file_provider/file_provider.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal:true
 
 require 'open-uri'
+require 'json'
+require 'http'
+require 'slack'
 
 # download file to `download_images/` directory
 # @param endpoint <string> image file link
@@ -18,4 +21,23 @@ def download_file(endpoint)
   end
 rescue OpenURI::HTTPError
   'リソース取得に失敗しました'
+end
+
+# upload file to slack API
+# https://api.slack.com/methods/files.upload
+# @param filepath
+# @param channel <string> sending channel('general', 'HOGE0A00Z'...)
+def upload_file(filepath, channel)
+  slack_api_key = ENV['SLACK_API_KEY']
+  Slack.configure do |config|
+    config.token = slack_api_key
+  end
+
+  ## upload file
+  Slack.files_upload(
+    file: Faraday::UploadIO.new(filepath, 'image/jpg'),
+    filename: File.basename(filepath),
+    filetype: File.extname(filepath),
+    channels: channel
+  )
 end
